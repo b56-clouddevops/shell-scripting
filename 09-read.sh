aa718317d1d3a381c5db21394929047a070a1f55
CodingManoj <mvp2612@hotmail.com>
2023-11-17 21:00:06 -0500

    AutoPush

--- 09-read.sh
+++ 09-read.sh
@@ -3,4 +3,3 @@
 read -p "Enter your name:" name
 echo "Name of the person is $name"
 
-echo '$? will show the exit code of the previous command'
---
+++ 10-functions.sh
@@ -0,0 +1,44 @@
+
+#!/bin/bash
+
+# There are 4 types of command available : 
+
+# 1) Binary                   ( /bin  , /sbin )
+# 2) Aliases                  ( Alises are shortcuts,  alias net="netstat -tulpn" )
+# 3) Shell Built-in Commands  
+# 4) Functions                # Functions are nothing but a set of command that can be written in a sequence and can be called n number of times as per your choice.
+
+
+# How to declare a function ?
+
+# f() {
+#     echo hai
+# }
+
+# How do you call a function 
+
+f   # calling a function
+
+# sample() {
+#     echo Hai from sample function
+#     echo Sample function is completed
+# }
+
+
+# sample 
+
+# echo sample call is completed 
+
+# sample 
+
+
+b56() {
+    echo "This is our batch56 function"
+    echo "We are learning functions"
+    echo "Todays date is:"
+    date 
+    echo "function b56 is completed"
+}
+
+
+b56 
