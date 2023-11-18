402e2c3d423edf37fb8c34f10911e8532f2291d0
CodingManoj <mvp2612@hotmail.com>
2023-11-17 19:43:11 -0500

    AutoPush

--- 02-esc-sequence.sh
+++ 02-esc-sequence.sh
@@ -3,6 +3,11 @@
 echo Welcome To Shell Scripting
 echo "This is day 1 of our shell scripting"
 
-echo "Line1\nLine2"
-echo Line3
-echo Line4 
+# Escape Sequence characters
+
+#  \n : New Line 
+# \t : tab Space 
+
+echo -e "Line1\nLine2"
+echo -e "Line3\tLine4"
+echo Line5
