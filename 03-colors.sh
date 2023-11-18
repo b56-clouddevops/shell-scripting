f483309320fa6ba40cb33b03d01b0e8557f09200
CodingManoj <mvp2612@hotmail.com>
2023-11-17 19:57:04 -0500

    AutoPush

--- 03-colors.sh
+++ 03-colors.sh
@@ -22,4 +22,4 @@ echo -e "\e[34m I am printing BLUE color \e[0m"
 echo -e "\e[35m I am printing MAGENTA color \e[0m"
 
 # Background + Foregroung
-echo -e "\e[43;34m  I am printng backgrond + foreground"
+echo -e "\e[43;34m  I am printng backgrond + foreground \e[0m"
