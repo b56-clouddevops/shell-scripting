cd269ce4a57bb56f1cfa5e22d2ec1184acb6013b
CodingManoj <mvp2612@hotmail.com>
2023-11-17 21:03:39 -0500

    AutoPush

--- 10-functions.sh
+++ 10-functions.sh
@@ -42,3 +42,13 @@ b56() {
 
 
 b56 
+
+
+stat() {
+
+    echo "Number of sessions opened $(who|wc -l)"
+    echo "Todays date is $(date +%F)"
+    echo "AVG Cpu Utilization In The Last 5 minues is : $(uptime|awk -F : '{print $NF}' | awk -F ',' '{print $2}')"
+
+    b56  # calling a function from another function
+}
