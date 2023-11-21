1ae82791e3190d4d6b4e4f62e2ae9c913bff7ce5
CodingManoj <thecloudcareers@gmail.com>
2023-11-20 20:36:25 -0500

    B56 Auto Push

--- 16-loops.sh
+++ 16-loops.sh
@@ -6,10 +6,13 @@
 #     1) For loop       ( When you know something to be executed n number of time, we use for loop)
 #     2) While loop     ( When you don't know something to be executed n number of times, we use while loop)
 
-# for loop syntaxt 
+# echo Value is 10
+# echo Value is 20 
+# echo Value is 30
+# echo Value is 40
+# echo Value is 50 
 
-echo Value is 10
-echo Value is 20 
-echo Value is 30
-echo Value is 40
-echo Value is 50 
+# for loop syntax 
+for i in 10 20 30 40 50 ; do 
+    echo "Vaules from the loops are $i"
+done 
