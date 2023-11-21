4c0703596b189389430d63f8ec8215b4d9118111
CodingManoj <thecloudcareers@gmail.com>
2023-11-20 20:22:30 -0500

    B56 Auto Push

--- 15-ifelse.sh
+++ 15-ifelse.sh
@@ -7,7 +7,11 @@ ACTION=$1
 if [ "$ACTION" == "start" ] ; then 
     echo -e "\e[32m Starting Shipping Service \e[0m"
     exit 0
+else 
+    echo -e "\e[31m Valid Option is start only \e[0m"
+    exit 1  
 fi 
 
 echo "It has not met any conditions"
 exit 100
+
