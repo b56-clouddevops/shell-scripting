2e6b801471f8cd27a3b6928fa2c51a650022d9c1
CodingManoj <thecloudcareers@gmail.com>
2023-11-20 21:09:00 -0500

    B56 Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -13,9 +13,9 @@ echo -e "***** \e[35m Configuring frontend \e[0m ******"
 echo "Installing Nginx :"
 yum install nginx -y      &>> /tmp/frontend.log
 if [ $? -eq 0 ] ; then 
-    echo -e "\e[31m Success \e[0m"
+    echo -e "\e[32m Success \e[0m"
 else 
-    echo -e "\e[32m Failure \e[0m"
+    echo -e "\e[31m Failure \e[0m"
 fi 
 
 
