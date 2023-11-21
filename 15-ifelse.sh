b1633bb1f6e6ef7ba50235e57b115b81a3476bf1
CodingManoj <thecloudcareers@gmail.com>
2023-11-20 20:11:44 -0500

    B56 Auto Push

--- 15-ifelse.sh
+++ 15-ifelse.sh
@@ -4,7 +4,7 @@ echo -e "Demo On If , If Else & Else If Usage"
 
 ACTION = $1 
 
-if [ $ACTION = start ] ; then 
+if [ $ACTION == start ] ; then 
     echo -e "\e[32m Starting Shipping Service \e[0m"
     exit 0
 fi 
