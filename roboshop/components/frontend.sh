8737e958eb1d4747cbd5a62844be2fd0e38cde13
CodingManoj <thecloudcareers@gmail.com>
2023-11-21 20:13:41 -0500

    Python Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -24,19 +24,18 @@ echo -n "Installing Nginx :"
 yum install nginx -y      &>> $LOGFILE
 stat $?
 
-echo -n "Downloading Component ${COMPONENT} :"
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-ls -ltr /tmp
+echo -n "Downloading the ${COMPONENT} component:"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" 
 stat $?
 
-echo -n "Cleanup of ${COMPONENT} : "
-rm -rf /usr/share/nginx/html/*     &>> $LOGFILE
+echo -n "Clean up of ${COMPONENT} : "
+cd /usr/share/nginx/html    
+rm -rf *     &>>  ${LOGFILE}
 stat $?
 
 echo -n "Extracting $COMPONENT :"
-pwd 
-ls -ltr
-unzip -o /tmp/frontend.zip    &>> $LOGFILE
+unzip -o /tmp/${COMPONENT}.zip    &>> $LOGFILE
+ls -ltr /tmp/${COMPOMENT}
 stat $?
 
 echo -n  "Configuring $COMPONENT :"
