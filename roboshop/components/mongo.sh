d4f72277b509fcac39952a50e167b9979a127c0c
CodingManoj <thecloudcareers@gmail.com>
2023-11-21 20:39:55 -0500

    Python Push

--- roboshop/components/mongo.sh
+++ roboshop/components/mongo.sh
@@ -30,7 +30,7 @@ yum install -y mongodb-org  &>> ${LOGFILE}
 stat $?
 
 echo -n "Enabling $COMPONENT visibility :"
-sed -i -e 's/127.0.0.1/0.0.0.0/' mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat $? 
 
 echo -n "Starting $COMPONENT :"
