ace1f456265231db49a81ca2916c237fc1229a5c
CodingManoj <thecloudcareers@gmail.com>
2023-11-21 20:34:48 -0500

    Python Push

--- roboshop/components/mongo.sh
+++ roboshop/components/mongo.sh
@@ -29,3 +29,12 @@ echo -n "Installing $COMPONENT :"
 yum install -y mongodb-org  &>> ${LOGFILE}
 stat $?
 
+echo -n "Enabling $COMPONENT visibility :
+sed -i -e 's/127.0.0.1/0.0.0.0/' mongod.conf
+stat $? 
+
+echo -n "Starting $COMPONENT :"
+systemctl enable mongo   &>> $LOGFILE
+systemctl daemon-reload  &>> $LOGFILE
+systemctl start mongod  &>> $LOGFILE
+stat $?
