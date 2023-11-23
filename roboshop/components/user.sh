c02e85e6cffe9e7ffc32040dc21a315c1957e159
CodingManoj <thecloudcareers@gmail.com>
2023-11-22 21:04:52 -0500

    B56 Auto Push

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -5,5 +5,6 @@ COMPONENT=user
 echo -e "***** \e[35m Configuring ${COMPONENT} \e[0m ******"
 
 source components/common.sh 
+NODEJS                              # Calling Nodejs Function
 
 echo -e "***** \e[35m $COMPONENT Configuration Is Complted \e[0m ******"
