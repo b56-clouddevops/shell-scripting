951a916cb010d151489fb93b6d4be3ebdba982ab
CodingManoj <thecloudcareers@gmail.com>
2023-11-24 19:53:39 -0500

    Python Push

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-USER_ID=$(id -u)
 COMPONENT=redis
-LOGFILE="/tmp/${COMPONENT}.log"
 
-stat() {
-    if [ $1 -eq 0 ] ; then 
-        echo -e "\e[32m Success \e[0m"
-    else 
-        echo -e "\e[31m Failure \e[0m"
-    fi 
-}
-
-if [ $USER_ID -ne 0 ] ; then 
-    echo -e "\e[31m This script is expected to be executed with sudo or as a root user \e[0m"
-    echo -e "\e[35m Example Usage: \n\t\t \e[0m sudo bash scriptName componentName"
-    exit 1 
-fi 
+source components/common.sh 
 
 echo -n "Configuring $COMPONENT repo :"
 curl -L https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/${COMPONENT}.repo -o /etc/yum.repos.d/${COMPONENT}.repo
