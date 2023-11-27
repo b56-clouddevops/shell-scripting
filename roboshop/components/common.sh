fa239d0f2e1f7a3505657eddc020976e159ab1f5
CodingManoj <thecloudcareers@gmail.com>
2023-11-26 20:10:18 -0500

    B56 Auto Push

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -141,6 +141,12 @@ PYTHON() {
     stat $?
 
     CONFIG_SVC
+
+    USERID=$(id -u roboshop)
+    GROUPID=$(id -g roboshop)
+
+    echo -n "Updating the uid and gid of the ${COMPONENT}.ini file"
+    sed -i -e "/^uid/ c uid=${USERID}" -e "/^gid/ c uid=${GROUPID}"  "${APPUSER_HOME}/${COMPONENT}.ini"
     
     START_SVC
 }
