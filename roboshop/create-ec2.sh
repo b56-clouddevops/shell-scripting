acd21e205eb9ed98459e429dcb76b084653a766f
CodingManoj <thecloudcareers@gmail.com>
2023-11-27 20:17:43 -0500

    B56 Auto Push

---
+++ roboshop/create-ec2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+# This script is going to create EC2 Servers
+
+# AMI_ID="ami-0f75a13ad2e340a58" # Hardcoding is a bad-choice particularly with AMI_ID as it's going be changed when you register a new AMI.
+# SGID="sg-052fd946b7e11841a"
+
+COMPONENT=$1
+HOSTEDZONEID="Z031297333JO38PNHPROR"
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=b56-allow-all" | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
+INSTANCE_TYPE="t3.micro"
+
+echo -e "******* \e[32m $COMPONENT \e[0m Server Creation In Progress ******* !!!!!!"
+PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}}]" | jq ".Instances[].PrivateIpAddress" | sed -e 's/"//g')
+
+echo -e "******* \e[32m $COMPONENT \e[0m DNS Record Creation In Progress ******* !!!!!!"
+sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/dns.json
+
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/dns.json
