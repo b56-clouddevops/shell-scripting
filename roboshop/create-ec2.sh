367df82ecd0c6f0bbb914e56d81ed38ea958046a
CodingManoj <thecloudcareers@gmail.com>
2023-11-27 20:28:15 -0500

    B56 Auto Push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -11,21 +11,30 @@ if [ -z $1 ] ; then
     exit 1 
 fi 
 
-
 COMPONENT=$1
 HOSTEDZONEID="Z031297333JO38PNHPROR"
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=b56-allow-all" | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
 INSTANCE_TYPE="t3.micro"
 
-echo -e "******* \e[32m $COMPONENT \e[0m Server Creation In Progress ******* !!!!!!"
-PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}}]" | jq ".Instances[].PrivateIpAddress" | sed -e 's/"//g')
-echo -e "******* \e[32m $COMPONENT \e[0m Server Creation Is Complted ******* !!!!!! \n\n"
+create_server() {
+    echo -e "******* \e[32m $COMPONENT \e[0m Server Creation In Progress ******* !!!!!!"
+    PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}}]" | jq ".Instances[].PrivateIpAddress" | sed -e 's/"//g')
+    echo -e "******* \e[32m $COMPONENT \e[0m Server Creation Is Complted ******* !!!!!! \n\n"
 
+    echo -e "******* \e[32m $COMPONENT \e[0m DNS Record Creation In Progress ******* !!!!!!"
+    sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/dns.json
 
-echo -e "******* \e[32m $COMPONENT \e[0m DNS Record Creation In Progress ******* !!!!!!"
-sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/dns.json
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/dns.json
+    echo -e "******* \e[32m $COMPONENT \e[0m DNS Record Creation Is Complted ******* !!!!!!"
+}
 
-aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/dns.json
-echo -e "******* \e[32m $COMPONENT \e[0m DNS Record Creation Is Complted ******* !!!!!!"
-
+# If the user supplies all as the first argument, then all these servers will be created.
+if [ "$1" == "all" ]; then 
+    for component in frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment; do 
+        COMPONENT=$component
+        create_server()
+    done
+else 
+    create_ec2 
+fi 
