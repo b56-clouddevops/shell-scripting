ea15965d2ab1d73bd90016b33b979773db48e4b2
CodingManoj <thecloudcareers@gmail.com>
2023-11-27 20:53:40 -0500

    B56 Auto Push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -5,13 +5,14 @@
 # AMI_ID="ami-0f75a13ad2e340a58" # Hardcoding is a bad-choice particularly with AMI_ID as it's going be changed when you register a new AMI.
 # SGID="sg-052fd946b7e11841a"
 
-if [ -z $1 ] ; then 
+if [ -z $1 ] || [ -z $2 ] ; then 
     echo -e "\e[31m ****** COMPONENT NAME IS NEEDED ****** \e[0m \n\t\t"
     echo -e "\e[36m \t\t Example Usage : \e[0m  bash create-ec2 ratings"
     exit 1 
 fi 
 
 COMPONENT=$1
+ENV=$2
 HOSTEDZONEID="Z031297333JO38PNHPROR"
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=b56-allow-all" | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
@@ -19,14 +20,16 @@ INSTANCE_TYPE="t3.micro"
 
 create_server() {
     echo -e "******* \e[32m $COMPONENT \e[0m Server Creation In Progress ******* !!!!!!"
-    PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}}]" | jq ".Instances[].PrivateIpAddress" | sed -e 's/"//g')
-    echo -e "******* \e[32m $COMPONENT \e[0m Server Creation Is Complted ******* !!!!!! \n\n"
 
-    echo -e "******* \e[32m $COMPONENT \e[0m DNS Record Creation In Progress ******* !!!!!!"
-    sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/dns.json
+    PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" | jq ".Instances[].PrivateIpAddress" | sed -e 's/"//g')
+    echo -e "******* \e[32m $COMPONENT-$ENV \e[0m Server Creation Is Complted ******* !!!!!! \n\n"
 
-    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/dns.json
-    echo -e "******* \e[32m $COMPONENT \e[0m DNS Record Creation Is Complted ******* !!!!!!"
+    echo -e "******* \e[32m $COMPONENT-$ENV \e[0m DNS Record Creation In Progress ******* !!!!!!"
+    sed -e "s/COMPONENT/${COMPONENT}-${ENV}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/dns.json
+
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/dns.json\
+
+    echo -e "******* \e[32m $COMPONENT \e[0m DNS Record Creation Is Complted ******* !!!!!! \n\n"
 }
 
 # If the user supplies all as the first argument, then all these servers will be created.
